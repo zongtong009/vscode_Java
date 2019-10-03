49f0050a091b2742c94fbcb1a1afd8b0f1caabda
zongtong009 <zongtongguilai1@gmail.com>
2019-10-04 00:22:41 +0800

    test20191004

--- hello.java
+++ hello.java
@@ -5,5 +5,6 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println(165465.2165);
+        System.out.println("Hello, World\n"+"The weather's good today");
     }
 }
